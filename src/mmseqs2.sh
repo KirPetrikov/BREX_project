f5cb76e26ad12dcb1a3a257bf7c02f8af39bd0e1
PKbioscience <bioscience.kp@gmail.com>
2025-04-13 16:52:50 +0300

    Add mmseqs2.sh v0.1 and mmseqs_clusters_table.py v0.1

---
+++ src/mmseqs2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#v0.1
+# $1 - path to input fasta-file
+# $2 - path to save results
+# $3 - N threads
+
+FASTA=$1
+RESULT=$2
+THREADS=$3
+
+mkdir -p "${RESULT}"/db
+
+mmseqs createdb "${FASTA}" "${RESULT}"/db/db
+
+mkdir "${RESULT}"/clust
+
+mmseqs cluster "${RESULT}"/db/db "${RESULT}"/clust/clust "${RESULT}"/mmseqs_tmp --min-seq-id 0.30 -c 0.8 --cov-mode 0 --threads $THREADS
+
+mmseqs createtsv "${RESULT}"/db/db "${RESULT}"/db/db "${RESULT}"/clust/clust "${RESULT}"/mmseqs_clusters.tsv --threads $THREADS
+
+echo '*** Create cluster table and summary histogram ***'
+
+python3 ~/bin/mmseqs_clusters_table.py -i "${RESULT}"/mmseqs_clusters.tsv -o "${RESULT}"
+
+echo '*** Finished ***'
+
