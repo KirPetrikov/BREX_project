a59b679916c9e7f84171776782ffb30e725a3638
PKbioscience <bioscience.kp@gmail.com>
2025-06-02 02:30:58 +0300

    Upd v0.1b: Add fields (EVal, Prob) & minors

--- src/parse_hhsuite_pdb70.py
+++ src/parse_hhsuite_pdb70.py
@@ -1,30 +1,40 @@
 """
-v0.1
+v0.1b
 """
 import json
+import re
 import sys
 import pandas as pd
 
 from pathlib import Path
 
+
 input_hh_suite_results = Path(sys.argv[1])
 input_n_hits = int(sys.argv[2])
 output_path = Path(sys.argv[3])
+prefix_for_save = input_hh_suite_results.name.lower()
 
-output_tsv_name = Path('pdb_top_annot.tsv')
-output_json_name = Path('pdb_annot.json')
+output_path.mkdir(parents=True, exist_ok=True)
+
+output_tsv_name = Path(f'{prefix_for_save}_pdb_top_annot.tsv')
+output_json_name = Path(f'{prefix_for_save}_pdb_annot.json')
 
 descriptions_catalog = {}  # Collections of all top annotations with descriptions
 only_first_annotations = {'Cluster': [],  # Top-1 annotations for table
-                          'pdb70_id': [],
-                          'Annotation': []}
+                          'PDB_ID': [],
+                          'PDB_ann': [],
+                          'EVal': [],
+                          'Prob': []}
+
+pattern_ann = re.compile(r'\d+ +(\w+) ')
 
 for file_name in input_hh_suite_results.iterdir():
     if file_name.suffix == '.hhr':
         with open(file_name) as file:
             cluster_id = file_name.stem
-            top_pdb_ids = []  # List of read top-hits ids
+            print(f'Parse {cluster_id}')
             annotations = []  # List of top-hits annotations
+            descriptions_catalog[cluster_id] = {}
 
             # Skip headers line
             count = 0
@@ -35,11 +45,10 @@ for file_name in input_hh_suite_results.iterdir():
             curr_line = file.readline()
 
             # Only top hit
-            curr_first_pdb_id = curr_line[4:11].strip(' ')
-            curr_first_annot = curr_line[11:36].strip(' ')
             only_first_annotations['Cluster'].append(cluster_id)
-            only_first_annotations['pdb70_id'].append(curr_first_pdb_id)
-            only_first_annotations['Annotation'].append(curr_first_annot)
+            only_first_annotations['PDB_ID'].append(re.search(pattern_ann, curr_line[:35]).group(1))
+            only_first_annotations['EVal'].append(float(curr_line[41:49].strip()))
+            only_first_annotations['Prob'].append(float(curr_line[35:41].strip()))
 
             # Top N hits with additional description
             count_n_hits = 0
@@ -48,21 +57,32 @@ for file_name in input_hh_suite_results.iterdir():
                     break
                 elif line.startswith('>'):
                     count_n_hits += 1
-                    curr_pdb_id = line[1:8].strip()
-                    curr_descr = '; '.join(line[8:].strip().split(';')[:-2])
-                    curr_e_val = file.readline().strip().split('  ')[1][8:]
-                    annotations.append((curr_pdb_id, curr_descr, curr_e_val))
+
+                    curr_pdb_id = line[1:10].strip()
+                    curr_descr = '; '.join(line[10:].strip().split(';')[:2])
+
+                    if count_n_hits == 1:
+                        only_first_annotations['PDB_ann'].append(curr_descr)
+
+                    stat_line = file.readline()
+                    curr_e_val = float(stat_line.split('  ')[1][8:])
+                    curr_prob = float(stat_line.strip().split('  ')[0][7:])
+
+                    curr_descriotion = {
+                        f'Hit_No_{count_n_hits}': {
+                            'PDB_ID': curr_pdb_id,
+                            'Description': curr_descr,
+                            'EVal': curr_e_val,
+                            'Prob': curr_prob
+                        }
+                    }
+                    descriptions_catalog[cluster_id].update(curr_descriotion)
+
                 else:
                     continue
 
-            all_top_annotations = {
-                f"Hit_No_{num + 1}": {'ID': ann[0], 'Description': ann[1], "E-val": ann[2]} for
-                num, ann in enumerate(annotations)}
-
-            descriptions_catalog[cluster_id] = all_top_annotations
-
 (pd.DataFrame.from_dict(only_first_annotations, orient='columns')
-   .to_csv(output_path / output_tsv_name, sep='\t', index=False))
+             .to_csv(output_path / output_tsv_name, sep='\t', index=False))
 
 with open(output_path / output_json_name, mode='w') as file:
     json.dump(descriptions_catalog, file)
